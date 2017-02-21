e0d0f3201223f41e4de69becb07cd5b5bfbce997
Kate Brown <kbrown8158@gmail.com>
2017-02-21 16:53:52 -0500

    arrays done

--- js/src/core/arrays.js
+++ js/src/core/arrays.js
@@ -30,22 +30,47 @@ concat = (arr1, arr2) => {
   return newArray
 };
 
-insert = (arr, item) => {
-
+insert = (arr, item, index) => {
+  arr.splice(index, 0, item);
+  return arr;
 }
 
 count = (arr, item) => {
-
+  total = 0;
+  for(let i = 0; i < arr.length; ++i) {
+    if(arr[i] == item)
+    total++;
+  }
+  return total
 };
 
 duplicates = (arr) => {
-
+  arr.sort();
+  let  newArray = []
+  for (var  i=0; i < arr.length; i++) {
+    if (arr[i + 1] == arr[i]) {
+      newArray.push(arr[i]);
+    }
+    if (newArray[newArray.length - 2] == arr[i]) {
+      newArray.splice(newArray.length - 1, 1)
+    }
+  }
+  return newArray;
 };
 
 square = (arr) => {
-
+  for (let i=0; i < arr.length; i++){
+    arr[i] = arr[i] * arr[i];
+  }
+  return arr;
 };
 
 findAllOccurrences = (arr, item) => {
-
+  let indexArray = [];
+  for(var i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      indexArray.push(i);
+    }
+  }
+  return indexArray;
 };
