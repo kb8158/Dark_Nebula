5a0aa91f13c8fbed2b71b51f6dec9c0b387ddcf0
Kate Brown <kbrown8158@gmail.com>
2017-02-22 21:44:33 -0500

    more parctice

--- js/src/core/functions.js
+++ js/src/core/functions.js
@@ -7,12 +7,16 @@ arraySum = (array) => {
 };
 
 fizzBuzz = (num) => {
-  // write a function that receives a number as its argument;
-  // if the number is divisible by 3, the function should return 'fizz';
-  // if the number is divisible by 5, the function should return 'buzz';
-  // if the number is divisible by 3 and 5, the function should return
-  // 'fizzbuzz';
-  //
-  // otherwise the function should return the number, or false if no number
-  // was provided or the value provided is not a number
+  if (num === null) {
+    return null;
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    return "fizzBuzz";
+  }
+  else if (num % 3 === 0) {
+    return "fizz";
+  }
+  else if (num % 5 === 0) {
+    return "buzz";
+  }
+  return num;
 };
