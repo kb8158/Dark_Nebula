37bb7e4a4fc1ab87fd6d8c082896d698032efb84
Kate Brown <kbrown8158@gmail.com>
2017-02-24 16:43:08 -0500

    strings

--- js/src/core/functions.js
+++ js/src/core/functions.js
@@ -1,9 +1,20 @@
 isPrime = (num) => {
+  for(let i = 2, i < num; i++) {
+    if  (num % i === 0) {
+      return false
+    } else {
+      return true
+    }
+  }
 
 };
 
 arraySum = (array) => {
-
+  count = 0;
+  for (var number of array){
+    count += number;
+  }
+  return count;
 };
 
 fizzBuzz = (num) => {
--- js/src/core/strings.js
+++ js/src/core/strings.js
@@ -1,7 +1,7 @@
 reduceString = (str, amount) => {
-
+  return str.slice(0, amount)
 };
 
 reverseString = (str) => {
-
+  return str.split('').reverse().join('');
 };
